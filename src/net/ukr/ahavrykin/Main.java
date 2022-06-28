b90dea56598179618b7a47949bdf401daa5078f2
AlexMnlH <AlexMnlH>
2022-06-28 19:13:00 +0300

    added deserialise

--- src/net/ukr/ahavrykin/Main.java
+++ src/net/ukr/ahavrykin/Main.java
@@ -3,41 +3,61 @@ package net.ukr.ahavrykin;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.util.Arrays;
+
 public class Main {
     static String JSON_TEXT = """
             {
-              “books”: [
+              "books": [
                  {
-                    “name”: “…”,
-                    “pages”: 100,
-                    “author”: “xxx”
+                    "name": "History of Ukraine",
+                    "pages": 100,
+                    "author": "xxx"
                  },
                  {
-                    “name”: “…”,
-                    “pages”: 200,
-                    “author”: “yyyy”
+                    "name": "Sсience",
+                    "pages": 200,
+                    "author": "yyyy"
                  }
               ]
             }
             """;
+
     public static void main(String[] args) {
-        Gson gson=new GsonBuilder().setPrettyPrinting().create();
-        Book books=gson.fromJson(JSON_TEXT,Book.class);
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        Books books = gson.fromJson(JSON_TEXT, Books.class);
         System.out.println(books);
+
+
+        String json= gson.toJson(books);
+        System.out.println(json);
+
     }
 }
 
-class Book{
+class Book {
     String name;
     int pages;
     String author;
 
     @Override
     public String toString() {
-        return "Book{" +
+        return '{' +
                 "name='" + name + '\'' +
                 ", pages=" + pages +
                 ", author='" + author + '\'' +
                 '}';
     }
 }
+
+class Books extends Book {
+    Book[] books;
+
+    @Override
+    public String toString() {
+        return "Books{" +
+                "books=" + Arrays.toString(books) +
+                '}';
+    }
+}
+
